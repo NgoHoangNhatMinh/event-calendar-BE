5a921220ea0af520922b926be7d4f349e7705ded
Minh <ngohoangnhatminh@gmail.com>
2025-02-22 13:03:18 +0800

    fix: update events controller to include all CRUD operations

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,26 +1,52 @@
 class EventsController < ApplicationController
-    def index
-        @events = Event.all
-        render json: @events
-      end
+  before_action :set_event, only: %i[show update destroy]
 
-      def show
-        @event = Event.find(params[:id])
-        render json: @event
-      end
+  # GET /events
+  def index
+    @events = Event.all
+    render json: @events
+  end
 
-      def create
-        @event = Event.new(event_params)
-        if @event.save
-          render json: @event, status: :created
-        else
-          render json: { errors: @event.errors.full_messages }, status: :unprocessable_entity
-        end
-      end
+  # GET /events/1
+  def show
+    render json: @event
+  end
 
-      private
+  # POST /events
+  def create
+    @event = Event.new(event_params)
 
-      def event_params
-        params.require(:event).permit(:name, :description, :time)
-      end
+    if @event.save
+      render json: @event, status: :created, location: @event
+    else
+      render json: @event.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /events/1
+  def update
+    if @event.update(event_params)
+      render json: @event
+    else
+      render json: @event.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /events/1
+  def destroy
+    @event.destroy
+    head :no_content
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def event_params
+    params.require(:event).permit(:name, :description, :date, :location)
+  end
 end
